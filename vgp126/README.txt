8649d4146ab90df7b758acbd5aec8e2737afa1cf
Michael Macasiano <aznmike123@gmail.com>
2012-10-08 21:06:32 -0400

    Added exercise 1-1
    Added exercise 1-1 as a test.

---
+++ vgp126/README.txt
@@ -0,0 +1,4 @@
+vgp126
+Winter 2011
+
+Introduction to programming class. We mainly covered C#.
---
+++ vgp126/bookwork/ex1-1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exercise1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What a great book!");
+        }
+    }
+}
